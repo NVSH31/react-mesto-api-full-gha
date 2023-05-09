f2fdde888d2a2ab27c8cdab4a7868c426db316dc
Natasha Shpak <n.v.shpak@yandex.ru>
2023-05-09 12:32:06 +0400

    rebuild

--- backend/utils/const.js
+++ backend/utils/const.js
@@ -3,6 +3,7 @@ const url = /^https?:\/\/\S{2,}\.[a-z]{2,3}\S*/;
 const allowedCors = [
   'http://shpaknv15.frontend.nomoredomains.monster',
   'https://shpaknv15.frontend.nomoredomains.monster',
+  'http://localhost:3000',
   'localhost:3000',
 ];
 
